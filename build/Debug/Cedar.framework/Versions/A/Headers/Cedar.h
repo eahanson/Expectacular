7b64afe25c77274c6776faa32710845490bedf2d
Erik Hanson <erik@eahanson.com>
2010-11-19 17:57:24 -0800

    remove build dir; add gitignore

--- build/Debug/Cedar.framework/Versions/A/Headers/Cedar.h
+++
@@ -1,7 +0,0 @@
-#if TARGET_OS_IPHONE
-#import <Cedar-iPhone/CDRFunctions.h>
-#import <Cedar-iPhone/CedarApplicationDelegate.h>
-#import <Cedar-iPhone/CDRExampleReporterViewController.h>
-#else
-#import <Cedar/CDRFunctions.h>
-#endif
